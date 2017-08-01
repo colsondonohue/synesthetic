cdd7f06d8e35b10b1e0c085eaeb1fbab95c1417a
Colson Donohue <colsondonohue@gmail.com>
2017-07-31 22:30:40 -0300

    Adjust color generation to include loudness is calculation

--- src/generateColors.js
+++ src/generateColors.js
@@ -1,19 +1,37 @@
 import chroma from 'chroma-js';
 
-const getMain = ({ acousticness, energy, valence, danceability, mode }) => {
-  const h = 1440 * Math.asin(acousticness) / Math.PI % 360;
+const getMain = ({
+  acousticness,
+  energy,
+  valence,
+  danceability,
+  loudness,
+  mode
+}) => {
+  const h = 720 * Math.asin((acousticness + 0.3) % 1) / Math.PI;
   const c = 30 * (energy + danceability);
-  // sqrt normalizes brightness so that colors don't get too muddy at low valences
-  const l = 90 * Math.sqrt(valence);
+  // cbrt normalizes brightness so that colors don't get too muddy at low valences
+  const l = 30 * Math.cbrt(10 + loudness + 20 * valence);
 
-  const main = chroma.hcl(h, c, l).hex();
+  console.log({
+    acousticness,
+    energy,
+    valence,
+    danceability,
+    loudness,
+    mode
+  });
+  console.log(`hcl: ${h} ${c} ${l}`);
   // cool down songs in minor mode
-  return mode === 1 ? chroma.mix(main, '#1b6ec2', 0.4, 'hcl') : main;
+  return mode === 0
+    ? chroma.hcl((h + 225) / 2, c, l).hex()
+    : chroma.hcl(h, c, l).hex();
 };
 
 const getSecondary = color => {
   const [h, c, l] = chroma(color).hcl();
-  return chroma.hcl((h + 180) % 360, c, l).hex();
+  const newL = l > 60 ? l - 10 : l + 10;
+  return chroma.hcl((h + 120) % 360, c, newL).hex();
 };
 
 const generateColors = songInfo => {
